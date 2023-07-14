fcee636b85202d8ac726be0f763d7e66a4144574
Alamy <mestafaalami11@gmail.com>
2023-07-14 13:38:20 +0000

    Add task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		};
+	};
+	putchar('\n');
+
+	return (0);
+}
