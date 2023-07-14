9994e0344c106f2f9ba9ae974902e6b3f09150a2
Alamy <mestafaalami11@gmail.com>
2023-07-14 13:26:24 +0000

    Add task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+	};
+	for (x = 'a'; x <= 'f'; x++)
+	{
+		putchar(x);
+	};
+	putchar('\n');
+
+	return (0);
+}
