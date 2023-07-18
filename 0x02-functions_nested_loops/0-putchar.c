c11297b3048a16b87b3fa6000bc833073dd5878e
Alamy <mestafaalami11@gmail.com>
2023-07-18 13:24:08 +0000

    Add READMEmd, main.h, task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * A program that prints _putchar, followed by a new line.
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
