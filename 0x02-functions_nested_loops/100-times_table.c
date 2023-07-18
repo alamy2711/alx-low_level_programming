7938bf240f86630174449e8d859bf858d69a018b
Alamy <mestafaalami11@gmail.com>
2023-07-18 21:10:29 +0000

    Fix 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,45 +10,48 @@ void print_times_table(int n)
 {
 	int i, j, m;
 
-	for (i = 0; i <= n; i++)
+	if (n >= 0 && n <= 15)
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			m = i * j;
-			if (j == 0)
-				_putchar(m + '0');
-			else if (m >= 100)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(m / 100 + '0');
-				_putchar((m / 10) % 10 + '0');
-				_putchar(m % 10 + '0');
+				m = i * j;
+				if (j == 0)
+					_putchar(m + '0');
+				else if (m >= 100)
+				{
+					_putchar(m / 100 + '0');
+					_putchar((m / 10) % 10 + '0');
+					_putchar(m % 10 + '0');
+				}
+				else if (m < 100 && m >= 10)
+				{
+					_putchar(m / 10 + '0');
+					_putchar(m % 10 + '0');
+				}
+				else if (m < 10)
+					_putchar(m % 10 + '0');
+				if (j != n && (m + i) < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else if (j != n && (m + i) >= 10 && (m + i) < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else if (j != n && (m + i) >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
-			else if (m < 100 && m >= 10)
-			{
-				_putchar(m / 10 + '0');
-				_putchar(m % 10 + '0');
-			}
-			else if (m < 10)
-				_putchar(m % 10 + '0');
-			if (j != n && (m + i) < 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-			}
-			else if (j != n && (m + i) >= 10 && (m + i) < 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-			}
-			else if (j != n && (m + i) >= 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
