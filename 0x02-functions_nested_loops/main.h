8b424b1d25da11bc61636f644f7d1cbbe1534a02
Alamy <mestafaalami11@gmail.com>
2023-07-18 13:53:03 +0000

    Fix 0-putchar.c main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
 #include <unistd.h>
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
 int _putchar(char);
 
 #endif /* MAIN_H */
