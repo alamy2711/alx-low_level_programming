0eaa86fb1472e659ae5cb0cc7511b52f203076fb
Alamy <mestafaalami11@gmail.com>
2023-07-18 19:48:29 +0000

    Add task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+/**
+ * add - adds two integers and returns the result.
+ * @a: the number to be added
+ * @b: the number to be added
+ *
+ * Return: Result of a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
