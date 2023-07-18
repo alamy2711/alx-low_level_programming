7f6b08eb174744a6cb56c722e938127a54a0e9a8
Alamy <mestafaalami11@gmail.com>
2023-07-18 14:21:01 +0000

    Add task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		_putchar(x);
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,5 +4,6 @@
 #include <unistd.h>
 
 int _putchar(char);
+void print_alphabet(void);
 
 #endif /* MAIN_H */
