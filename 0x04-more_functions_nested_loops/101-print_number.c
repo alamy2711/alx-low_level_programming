2eb770e374a8dee032a9864119b69d9c721e3192
Alamy <mestafaalami11@gmail.com>
2023-07-20 20:34:46 +0000

    Fix 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,28 +7,29 @@
 */
 void print_number(int n)
 {
-	unsigned int l, holder;
+	unsigned int l, holder, pr;
 
 	l = 1;
-	holder = n;
-
 
 	if (n < 0)
 	{
 		n *= -1;
 		_putchar('-');
 	}
+	holder = n;
 
 	do {
 		holder /= 10;
 		l *= 10;
 	} while (holder != 0);
-
 	l /= 10;
+
+	pr = n;
 
 	while (l != 0)
 	{
-		_putchar((n / l) % 10 + '0');
+		_putchar((pr / l) % 10 + '0');
 		l /= 10;
 	}
 }
+
