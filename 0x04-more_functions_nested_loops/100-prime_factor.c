4f0ad4267bd6e78d84d8d9e11e06bb4886b01a33
Alamy <mestafaalami11@gmail.com>
2023-07-20 16:14:43 +0000

    Fix 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -11,44 +11,13 @@ int isPrime(int num);
 */
 int main(void)
 {
-	const unsigned long int NUM = 612852475143;
-	unsigned long int i, lp;
+	unsigned long i, N = 612852475143;
 
-	for (i = 2; i <= NUM; i++)
-	{
-		if (isPrime(i) && NUM % i == 0)
-		{
-			lp = i;
-			/*printf("%lu\n", lp);*/
-		}
-	}
-	printf("%lu\n", lp);
+	for (i = 2; i * i <= N; i++)
+		while (N % i == 0)
+			N /= i;
+	printf("%lu\n", N);
 
 	return (0);
 }
 
-/**
- * isPrime - checks if a number is prime
- * @n: number to check
- * Return: 1 if n is prime, 0 if n is not prime
-*/
-int isPrime(int n)
-{
-	int i;
-
-	if (n <= 1)
-		return (0);
-	if (n <= 3)
-		return (1);
-
-	if (n % 2 == 0 || n % 3 == 0)
-		return (0);
-
-	for (i = 5; i * i <= n; i = i + 6)
-	{
-		if (n % i == 0 || n % (i + 2) == 0)
-			return (0);
-	}
-
-	return (1);
-}
