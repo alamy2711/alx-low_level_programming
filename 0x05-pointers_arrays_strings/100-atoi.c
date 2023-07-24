0372d570a6284af4b08e361878e8a9d59dea4e40
Alamy <mestafaalami11@gmail.com>
2023-07-24 22:46:01 +0000

    Fix 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,13 +8,24 @@
 */
 int _atoi(char *s)
 {
-	int i, res = 0;
+	int i, sign = 0, res = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if(s[i] >= '0' && s[i] <= '9')
+		if (s[i] == '-')
+			sign++;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
 			res = (res * 10) + (s[i] - '0');
+
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+		}
 	}
+
+	if (sign % 2 == 1)
+		return (-res);
 
 	return (res);
 }
