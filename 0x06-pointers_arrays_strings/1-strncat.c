c251bf7de97634e44f3bb2299f29f53b19c82e54
Alamy <mestafaalami11@gmail.com>
2023-07-26 15:44:06 +0000

    Fix task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,10 +14,10 @@ char *_strncat(char *dest, char *src, int n)
 	while (dest[dest_len] != '\0')
 		dest_len++;
 
-	while (i++ < n && src[i] != '\0')
+	while (i++ < n && src[i - 1] != '\0')
 		dest[dest_len + i - 1] = src[i - 1];
 
-	dest[dest_len + i - 1] = '\0';
+	dest[dest_len + i -1] = '\0';
 
 	return (dest);
 }
