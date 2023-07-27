80b68d7a22a5f57167a23e82481c19ea5145cb74
Alamy <mestafaalami11@gmail.com>
2023-07-27 14:42:05 +0000

    Fix 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,34 +1,50 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-* print_buffer - prints a buffer
- * @b: the address of the memory to print
- * @size: the size of the memory to print
- */
+* print_buffer - prints buffer
+* @b: buffer
+* @size: size
+* Return: void
+*/
+
 void print_buffer(char *b, int size)
 {
-	int offset, byte_index, char_index;
+int o, j, i;
 
-	for (offset = 0; offset < size; offset += 10)
-	{
-		/* print offset */
-		printf("%08x: ", offset);
+o = 0;
 
-		/* print bytes in hex */
-		for (byte_index = 0; byte_index < 10; byte_index++)
-		{
-			(offset + byte_index < size) ? printf("%02x", b[offset + byte_index])
-				: printf("  ");
-			(byte_index % 2) ? printf(" ") : 0;
-		}
+if (size <= 0)
+{
+printf("\n");
+return;
+}
+while (o < size)
+{
+j = size - o < 10 ? size - o : 10;
+printf("%08x: ", o);
+for (i = 0; i < 10; i++)
+{
+if (i < j)
+printf("%02x", *(b + o + i));
+else
+printf("  ");
+if (i % 2)
+{
+printf(" ");
+}
+}
+for (i = 0; i < j; i++)
+{
+int c = *(b + o + i);
 
-		/* print bytes in ascii */
-		for (char_index = 0; char_index < 10; char_index++)
-			(offset + char_index < size) ? printf("%c", (b[offset + char_index] >= 32 &&
-				b[offset + char_index] <= 126) ? b[offset + char_index] : '.') : 0;
-
-		printf("\n");
-	}
-
-	(size <= 0) ? printf("\n") : 0;
+if (c < 32 || c > 132)
+{
+c = '.';
+}
+printf("%c", c);
+}
+printf("\n");
+o += 10;
+}
 }
