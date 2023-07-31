6c4329f74251733c446cee8847f928f964883b2c
Alamy <mestafaalami11@gmail.com>
2023-07-31 15:12:19 +0000

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,11 +11,14 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+	
+	if (s != NULL)
 	{
-		if (s[i] == c)
-			return (s + i);
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			if (s[i] == c)
+				return (s + i);
+		}
 	}
 	return (NULL);
 }
