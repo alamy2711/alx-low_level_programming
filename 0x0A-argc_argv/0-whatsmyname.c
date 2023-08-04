ab14fd27dd5179b70ca1b02813f2d8ca570c12f0
Alamy <mestafaalami11@gmail.com>
2023-08-04 19:06:13 +0000

    Add 0-whatsmyname.c and README.md

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints its name
+ * @argc: arguments count
+ * @argv: arguments vector
+ * Return: Always 0
+*/
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
