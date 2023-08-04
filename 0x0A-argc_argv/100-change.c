a796ae294f1dea409bd7d58172d1e4cff1be1e8f
Alamy <mestafaalami11@gmail.com>
2023-08-04 20:31:53 +0000

    Add 100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - A program that adds positive numbers
+ * @argc: arguments count
+ * @argv: arguments vector
+ * Return: 1 if argc != 2, otherwise 0
+*/
+int main(int argc, char *argv[])
+{
+	int i, amt, coins = 0;
+	int coins_values[5] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (0);
+	}
+
+	amt = atoi(argv[1]);
+
+	for (i = 0; i < 5; i++)
+	{
+		while (amt >= coins_values[i])
+		{
+			coins += amt / coins_values[i];
+			amt %= coins_values[i] * (amt / coins_values[i]);
+		}
+	}
+	printf("%d\n", coins);
+
+	return (0);
+}
