fae504a99cb56c4a409522ce42ee6e7ac2425046
Alamy <mestafaalami11@gmail.com>
2023-08-07 20:10:44 +0000

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -17,7 +17,7 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 
 	/* Calculate total length */
-	for (i = 1; i < ac; i++)
+	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 			len++;
