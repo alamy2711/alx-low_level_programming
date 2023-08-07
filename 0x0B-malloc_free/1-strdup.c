dea0699d655737aa06422a8417cb900dc8ee95e9
Alamy <mestafaalami11@gmail.com>
2023-08-07 16:01:13 +0000

    Add 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates a string
+ * a specific char
+ * @str: string to duplicate
+ * Return: NULL if str = NULL or malloc failed, otherwise returns copy of str
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int i, len = 0;
+
+	/* Calculate str's length */
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
+
+	s = malloc(len * sizeof(char) + 1);
+
+	if (s == NULL || len == 0)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		s[i] = str[i];
+
+	s[len] = '\0';
+
+	return (s);
+}
