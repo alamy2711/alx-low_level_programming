9ed535e7d2e647ab9f44d4fba58dc34d994fea92
Alamy <mestafaalami11@gmail.com>
2023-08-07 20:11:45 +0000

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -28,7 +28,7 @@ char *argstostr(int ac, char **av)
 	if (str == NULL)
 		return (NULL);
 
-	for (i = 1; i < ac; i++)
+	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 			str[tracker++] = av[i][j];
