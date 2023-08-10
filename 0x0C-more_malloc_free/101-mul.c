944d4d1192cf80dce348237b0bcbd3f40dd91098
Alamy <mestafaalami11@gmail.com>
2023-08-10 23:52:56 +0000

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -6,7 +6,7 @@
 
 /* Prototype */
 void print_error_and_exit(void);
-void print_reversed_string(char *str);
+void print_array(int *arr, int len);
 
 /**
  * main - A program that multiplies two positive numbers
@@ -18,7 +18,7 @@ void print_reversed_string(char *str);
 int main(int ac, char *av[])
 {
 	char *num1, *num2;
-	char *res;
+	int *res;
 	int len1, len2;
 	int i, j, k, dig1, dig2, sum, carry;
 
@@ -32,13 +32,9 @@ int main(int ac, char *av[])
 	len2 = strlen(num2);
 
 	/* Allcoate a block of memory to store the result */
-	res = malloc((len1 + len2 + 1) * sizeof(char));
+	res = calloc(len1 + len2, sizeof(int));
 	if (res == NULL)
 		print_error_and_exit();
-
-	/* Initialize res with zeros */
-	for (i = 0; i < len1 + len2 + 1; i++)
-		res[i] = '0';
 
 	/* Calcualate Multiplication */
 	for (i = len1 - 1; i >= 0; i--)
@@ -54,37 +50,22 @@ int main(int ac, char *av[])
 			dig1 = num1[i] - '0';
 			dig2 = num2[j] - '0';
 
-			k = (len1 - i - 1) + (len2 - j - 1);
-			sum = dig1 * dig2 + (res[k] - '0') + carry;
+			k = i + j + 1;
+			sum = dig1 * dig2 + res[k] + carry;
 
 			carry = sum / 10;
 
-			res[k] = (sum % 10) + '0';
+			res[k] = (sum % 10);
 		}
 		if (carry > 0)
 		{
-			k = (len1 - i - 1) + (len2 - j - 1);
+			k = i + j + 1;
 			res[k] += carry;
 		}
 	}
 
-	/* Skip zeros from the right */
-	for (i = len1 + len2 - 1; res[i] == '0' && i >= 0;)
-		i--;
-
-	/* Checks if result is 0 */
-	if (i == -1)
-	{
-		_putchar('0');
-		_putchar('\n');
-		free(res);
-		return (0);
-	}
-
-	res[i + 1] = '\0';
-
-	/* Print the reversed result */
-	print_reversed_string(res);
+	/* Print the result */
+	print_array(res, len1 + len2);
 
 	free(res);
 
@@ -106,15 +87,27 @@ void print_error_and_exit(void)
 }
 
 /**
- * print_reversed_string - Prints a string in reverse
- * @str: String to print in reverse
+ * print_array - Prints an array
+ * @arr: Array to print
+ * @len: Array length
  */
-void print_reversed_string(char *str)
+void print_array(int *arr, int len)
 {
 	int i;
 
-	for (i = strlen(str) - 1; i >= 0; i--)
-		_putchar(str[i]);
+	/* Skip zeros from the left */
+	for (i = 0; arr[i] == 0 && i < len;)
+		i++;
+
+	/* Checks if arr contains only zeros */
+	if (i == len)
+	{
+		_putchar('0');
+		_putchar('\n');
+	}
+
+	for (; i < len; i++)
+		_putchar(arr[i] + '0');
 
 	_putchar('\n');
 }
