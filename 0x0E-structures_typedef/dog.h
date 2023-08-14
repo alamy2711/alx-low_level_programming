ad7b0a9ce0f25f95a29c3279d5b03913952dd783
Alamy <mestafaalami11@gmail.com>
2023-08-14 21:47:33 +0000

    Add dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Contains a dog information
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+*/
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+
+#endif /* DOG_H */
