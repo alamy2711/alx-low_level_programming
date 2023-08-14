a257d6e68736b3bd12500d97c476f2299d94b425
Alamy <mestafaalami11@gmail.com>
2023-08-14 23:45:43 +0000

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,30 +13,29 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	char *dog_name, *dog_owner;
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 
-	dog_name = malloc((strlen(name) + 1) * sizeof(char));
-	if (dog_name == NULL)
+	dog->name = malloc((strlen(name) + 1) * sizeof(char));
+	if (dog->name == NULL)
 	{
-		free(dog_name);
+		free(dog->name);
 		return (NULL);
 	}
 
-	dog_owner = malloc((strlen(owner) + 1) * sizeof(char));
-	if (dog_owner == NULL)
+	dog->owner = malloc((strlen(owner) + 1) * sizeof(char));
+	if (dog->owner == NULL)
 	{
-		free(dog_name);
-		free(dog_owner);
+		free(dog->name);
+		free(dog->owner);
 		return (NULL);
 	}
 
-	dog->name = strcpy(dog_name, name);
+	strcpy(dog->name, name);
 	dog->age = age;
-	dog->owner = strcpy(dog_owner, owner);
+	strcpy(dog->owner, owner);
 
 	return (dog);
 }
