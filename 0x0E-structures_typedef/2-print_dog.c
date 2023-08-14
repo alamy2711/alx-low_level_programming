27fca924e6b385aa83345db6e646bded102d5d69
Alamy <mestafaalami11@gmail.com>
2023-08-14 22:25:21 +0000

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,16 +12,16 @@ void print_dog(struct dog *d)
 		if (d->name)
 			printf("Name: %s\n", d->name);
 		else
-			printf("Name: (nil)");
+			printf("Name: (nil)\n");
 
 		if (d->age && d->age != 0)
 			printf("Age: %f\n", d->age);
 		else
-			printf("Age: (nil)");
+			printf("Age: (nil)\n");
 
 		if (d->owner)
 			printf("Owner: %s\n", d->owner);
 		else
-			printf("Owner: (nil)");
+			printf("Owner: (nil)\n");
 	}
 }
