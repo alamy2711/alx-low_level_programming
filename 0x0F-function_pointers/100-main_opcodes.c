21e59d86d4840514048c8af8373217318fc5fabf
Alamy <mestafaalami11@gmail.com>
2023-08-16 21:44:03 +0000

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -3,18 +3,24 @@
 
 /**
  * main - A program that prints the opcodes of its own main function
+ * @ac: Arguments count
+ * @av: Arguments vector
+ *
+ * Return: 0 (Success)
  */
-int main (int ac, char *av[])
+int main(int ac, char *av[])
 {
 	int i, bytes;
 	int (*main_ptr)(int, char **);
 
+	/* Checks number of Arguments */
 	if (ac != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
 
+	/* Checks number of bytes */
 	bytes = atoi(av[1]);
 	if (bytes < 0)
 	{
@@ -22,15 +28,15 @@ int main (int ac, char *av[])
 		exit(2);
 	}
 
+	/* Printing opcode */
 	main_ptr = main;
-
 	for (i = 0; i < bytes; i++)
 	{
 		printf("%02x", *((unsigned char *)main_ptr++));
 		if (i != bytes - 1)
 			printf(" ");
 	}
-	printf ("\n");
+	printf("\n");
 
 	return (0);
 }
