8fac50a12c917564d14688e0b5eca53108d6f2d6
Alamy <mestafaalami11@gmail.com>
2023-08-24 18:46:00 +0000

    Add 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * print_sentence - Prints a sentence;
+ */
+void print_sentence(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	"I bore my house upon myback!\n");
+}
