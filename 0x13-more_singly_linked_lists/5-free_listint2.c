ed49cc72777632062c0c6e3a390e7a1c60c57d67
Alamy <mestafaalami11@gmail.com>
2023-08-28 20:49:58 +0100

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,7 +8,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return;
 
 	while (*head != NULL)
@@ -17,6 +17,4 @@ void free_listint2(listint_t **head)
 		*head = (*head)->next;
 		free(temp);
 	}
-
-	*head = NULL;
 }
