ad91507fbd3011d691e4070ad22539eb06995916
Alamy <mestafaalami11@gmail.com>
2023-08-31 17:18:29 +0100

    Add 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,5 +9,10 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	int numOfBits = sizeof(unsigned long int) * 8;
+
+	if (index >= numOfBits || index < numOfBits)
+		return (-1);
+
 	return ((n >> index) & 1);
 }
