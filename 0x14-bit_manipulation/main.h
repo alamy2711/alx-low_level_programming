90befe57eda5f23a443467e190081600bd160198
Alamy <mestafaalami11@gmail.com>
2023-08-31 16:20:37 +0100

    Add main.h

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Headers*/
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+/* Prototypes */
+unsigned int binary_to_uint(const char *b);
+
+#endif /* MAIN_H */
