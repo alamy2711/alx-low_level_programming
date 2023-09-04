ccfe3970c289f478fd600638d22fd6c51f5b53c4
Alamy <mestafaalami11@gmail.com>
2023-09-05 00:15:09 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,12 +17,24 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
+	{
+		close(fd);
 		return (-1);
+	}
+
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
 
 	write_return = write(fd, text_content, strlen(text_content));
 
 	if (write_return == -1)
+	{
+		close(fd);
 		return (-1);
+	}
 
 	close(fd);
 
