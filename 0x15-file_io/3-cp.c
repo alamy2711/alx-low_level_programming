17d2e47fffb4ca141e48ade963ce63bc5e3e91dc
Alamy <mestafaalami11@gmail.com>
2023-09-05 01:26:46 +0100

    Add 3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,73 @@
+#include "main.h"
+
+/* prototypes*/
+void printCloseError(int close_return);
+
+/**
+ * main - A program that copies the content of a file to another file
+ * @ac: Arguments count
+ * @av: Arguments vector
+ *
+ * Return: 0
+ */
+int main(int ac, char **av)
+{
+	int fd_from, fd_to, numLetters = 1, write_return;
+	char *buffer[1024];
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd_from = open(av[1], O_RDWR, 0664);
+	fd_to = open(av[2], O_RDWR | O_CREAT | O_APPEND | O_TRUNC, 0664);
+
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s", av[1]);
+		exit(98);
+	}
+
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s", av[2]);
+		exit(99);
+	}
+
+	while ((numLetters = read(fd_from, buffer, 1024)) > 0)
+	{
+		write_return = write(fd_to, buffer, numLetters);
+
+		if (write_return == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s", av[2]);
+			exit(99);
+		}
+	}
+
+	if (numLetters == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s", av[1]);
+		exit(98);
+	}
+
+	printCloseError(close(fd_from));
+	printCloseError(close(fd_to));
+
+	return (0);
+}
+
+/**
+ * printCloseError - Prints the an Error if the close function failed
+ * @close_return: Close function's return
+ */
+void printCloseError(int close_return)
+{
+	if (close_return == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", close_return);
+		exit(100);
+	}
+}
