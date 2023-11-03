e5192d418250a0139cc789f6b7c207458e0b3fc8
Alamy <mestafaalami11@gmail.com>
2023-11-03 22:37:28 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,7 +14,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	table->size = size;
-	table->array = NULL;
+	table->array = calloc(size, sizeof(hash_node_t));
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
 
 	return (table);
 }
