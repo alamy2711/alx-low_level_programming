320ec39f6211914c88a077cd9701620163dba9ff
Alamy <mestafaalami11@gmail.com>
2023-11-03 22:43:15 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,16 +15,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	table->size = size;
-	table->array = calloc(size, sizeof(hash_node_t));
+	table->array = calloc(size, sizeof(hash_node_t *));
 
 	if (table->array == NULL)
 	{
 		free(table);
 		return (NULL);
 	}
-
-	for (i = 0; i < size; i++)
-		table->array[i] = NULL;
 
 	return (table);
 }
