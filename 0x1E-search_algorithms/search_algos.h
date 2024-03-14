3c6f47ebb764c56a8547494600f9d39b34769d62
Alamy <mestafaalami11@gmail.com>
2024-03-14 06:22:40 +0000

    Add search_algos.h

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/* Headers */
+#include <stdio.h>
+
+/* Prototypes */
+int linear_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_ALGOS_H */
